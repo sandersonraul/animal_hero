2d517f1d8e7ee5d52629f9c4d97984b0f6c27751
Raul Oliveira <74179733+sandersonraul@users.noreply.github.com>
2021-06-28 11:21:37 -0300

    Criando relação User  e Ong

--- db/schema.rb
+++ db/schema.rb
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 2020_09_27_041742) do
     t.string "logo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_ongs_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
