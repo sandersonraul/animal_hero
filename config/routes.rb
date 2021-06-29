222f130d2481c391e7eb066da546642cc740ab27
Raul Oliveira <74179733+sandersonraul@users.noreply.github.com>
2021-06-29 18:16:51 -0300

    Atualizando rota ongs/index

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   get 'entrar', to:'sessions#new'
   post 'entrar', to:'sessions#create'
   delete 'sair', to:'sessions#destroy'
-  root to:"home#index"
+  root to:"ongs#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
