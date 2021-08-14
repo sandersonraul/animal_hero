e6803928da00579e6dc313ef806a0de2ab984951
FabioRocha462 <rochafabio462@gmail.com>
2021-08-14 17:39:03 -0300

    Editar Funcioando

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,38 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
   def new
     @user = User.new
   end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to @user, notice: 'User atualizada com sucesso' }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /user/1
+  # DELETE /user/1.json
+  def destroy
+    @user.destroy
+    respond_to do |format|
+      format.html { redirect_to users_url, notice: 'user was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  
 
   def create
     @user = User.create(user_params)
@@ -12,6 +43,12 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(params[:id])
+    end
 
   private
     def user_params
